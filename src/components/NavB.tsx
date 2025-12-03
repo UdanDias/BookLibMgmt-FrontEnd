341620498c678f86f6c56cd956555320b1c494fc
UdanDias <udandias892@gmail.com>
2025-12-03 15:48:16 +0530

    created Nav bar

---
+++ src/components/NavB.tsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="#book">Book</Nav.Link>
+            <Nav.Link href="#staff">Staff</Nav.Link>
+            <Nav.Link href="#member">Member</Nav.Link>
+            <Nav.Link href="#lending">Lending</Nav.Link>
+
+          </Nav>
+        </Container>
+      </Navbar>
+      
+    </>
+  );
+}
+
+export default ColorSchemesExample;

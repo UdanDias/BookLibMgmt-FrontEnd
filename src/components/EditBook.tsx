e3172de4a82a2cd72bb3141b41cafe7f48c60344
UdanDias <udandias892@gmail.com>
2025-12-04 11:57:13 +0530

    added model for editing books

--- src/components/EditBook.tsx
+++ src/components/EditBook.tsx
@@ -26,25 +26,22 @@ export const EditBook :React.FC<BookEditProps>=({show,selectedRow,handleOnClose,
 
     return (
         <>
-            <div
-                className="modal show"
-                style={{ display: 'block', position: 'initial' }}
-                >
-                <Modal.Dialog>
-                    <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
-                    </Modal.Header>
+      
 
-                    <Modal.Body>
-                    <p>Modal body text goes here.</p>
-                    </Modal.Body>
-
-                    <Modal.Footer>
-                    <Button variant="secondary">Close</Button>
-                    <Button variant="primary">Save changes</Button>
-                    </Modal.Footer>
-                </Modal.Dialog>
-            </div>
-        </>
+      <Modal show={show} onHide={handleOnClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleOnClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleOnClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
     );
 }
